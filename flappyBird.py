36b175a6306d7697c373496b3cbcf8efacc3a440
Teddy Macbook <teddysmoot@gmail.com>
2021-03-01 04:39:34 -0500

    fixed width

--- flappyBird.py
+++ flappyBird.py
@@ -5,7 +5,7 @@ import os
 import random
 #flapp
 
-WIN_WIDTH = 600
+WIN_WIDTH = 500
 WIN_HEIGHT = 800
 
 BIRD_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird1.png"))), pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird2.png"))), pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird3.png")))]
